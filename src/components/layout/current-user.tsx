feced9a18652050477b05ed57fb4beaec44b0fb9
Grover Soans <groverpsoans@gmail.com>
2024-11-19 19:07:56 -0600

    Created and added utility functions

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -6,6 +6,8 @@ import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
     const {data: user} = useGetIdentity<User>()
+
+    
   return (
     <>
         <Popover
@@ -14,7 +16,13 @@ const CurrentUser = () => {
             overlayInnerStyle={{padding:0 }}
             overlayStyle={{ zIndex: 999 }}
         >
-            <CustomAvatar/>
+            <CustomAvatar
+                name={user?.name}
+                src={user?.avatarUrl}
+                size='default'
+                style={{cursor:'pointer'}}
+            
+            />
         </Popover>
     
     </>
